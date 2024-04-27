bf6d79519134afd786d5f7d2c68709e4443b498c
KIMSIHUN <tlgns14@nate.com>
2024-04-27 14:59:41 +0900

    🎨 레이아웃 푸터 컴포넌트추가

---
+++ src/app/components/semantic/footer.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent } from 'react'
+import { Mail, Github } from 'lucide-react'
+interface FooterProps {}
+
+const Footer: FunctionComponent<FooterProps> = ({}) => {
+  return (
+    <footer className="footer flex justify-between bg-white p-10">
+      <aside>
+        <p>© 2024 Sihun.</p>
+      </aside>
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          <a href="mailto:tlgns14@nate.com" className="flex flex-row space-x-2">
+            <Mail />
+          </a>
+          <a
+            href="https://github.com/sihun0105"
+            target="_blank"
+            className="flex flex-row space-x-2"
+          >
+            <Github />
+          </a>
+        </div>
+      </nav>
+    </footer>
+  )
+}
+
+export default Footer
